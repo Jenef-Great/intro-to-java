f5f184e3a216cf5454802f255411eec3ddba4f4d
Jenef-Great <jenef-great.alemonai@amalitech.com>
2025-08-25 16:43:12 +0000

    return value from method

--- src/lab/Mortgage.java
+++ src/lab/Mortgage.java
@@ -9,14 +9,16 @@ package lab;
 
 public class Mortgage {
     public static void main(String args[]) {
-        calcMortgage(100000, 4.2, (byte)30);
+
+        double mortgageAmount = calcMortgage(100000, 4.2, (byte)30);
+        System.out.println("mortgage amount: " + mortgageAmount);
     }
 
-    private static void calcMortgage(int principal, double rate, byte time) {
+    private static double calcMortgage(int principal, double rate, byte time) {
         double monthlyRate = rate / 100 / 12;
         int totalPayments = time * 12;
         double amount = principal * (monthlyRate * Math.pow(1 + monthlyRate, totalPayments)) / (Math.pow(1 + monthlyRate, totalPayments) - 1);
 
-        System.out.println(amount);
+        return amount;
     }
 }
